bfcefd182b4894cd7d359b8bd927db9be238cb77
yxu <yxu@suse.de>
2017-08-30 10:27:07 +0200

    WIP

--- tests/console/type_string_test.pm
+++ tests/console/type_string_test.pm
@@ -4,10 +4,29 @@ use testapi;
  
 sub run {
     select_console 'root-console';
-    type_string ("Do not wait_still_screen", 25, 0, 0);
-    type_string ("wait_still_screen for 5 seconds", 25, 0, 5);
-    type_string ("wait_still_screen for 10 seconds", 25, 0, 10);
-    type_string ("wait_still_screen for 20 seconds", 25, 0, 20);
+    type_string ("date\n");
+    wait_still_screen 1;
+    save_screenshot;
+    type_string ("Do not wait_still_screen\n", max_interval => 100, wait_screen_changes => 10, wait_still_screen => 0);
+    type_string ("date\n");
+    wait_still_screen 1;
+    save_screenshot;
+    type_string ("wait_still_screen for 5 seconds\n",max_interval =>  100, wait_screen_changes => 10, wait_still_screen => 5);
+    type_string ("date\n");
+    wait_still_screen 1;
+    save_screenshot;
+    type_string ("wait_still_screen for 10 seconds\n", max_interval => 50, wait_screen_changes => 20, wait_still_screen => 10);
+    type_string ("date\n");
+    wait_still_screen 1;
+    save_screenshot;
+    type_string ("wait_still_screen for 20 seconds\n", max_interval => 125, wait_screen_changes => 10, wait_still_screen => 20);
+    type_string ("date\n");
+    wait_still_screen 1;
+    save_screenshot;
+    type_string ("wait_still_screen for 30 seconds\n", max_interval => 250, wait_screen_changes => 0, wait_still_screen => 30);
+    type_string ("date\n");
+    wait_still_screen 1;
+    save_screenshot;
 }
 
 1;
